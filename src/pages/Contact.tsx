345c4c948eca2fee39c4825ea02f5d212e043a13
aristidei <a.isingizwe@alustudent.com>
2024-02-16 17:58:13 +0200

    faq section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,11 @@
+import Contact from "../components/contact/Contact";
+import Faq from "../components/contact/Faq";
+
 export default function ContactPage() {
-  return <h1>contact page</h1>;
+  return (
+    <article className="px-16 md:px-6 sm:px-3 my-16">
+      <Contact />
+      <Faq />
+    </article>
+  );
 }
